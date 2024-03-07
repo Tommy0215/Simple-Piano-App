266fd1d52bbff7c3b3272bbce67539cfd3886bc0
Zhixian XU <zhixianx@student.unimelb.edu.au>
2024-03-07 21:18:08 +1100

    Initial Commit

---
+++ Piano/PianoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoApp.swift
+//  Piano
+//
+//  Created by Zhixian Xu on 7/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct PianoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
