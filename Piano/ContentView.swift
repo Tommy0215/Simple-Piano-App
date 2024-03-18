d123d6aada2ec697535cd7e0ef01b76dc55eb492
Tommy0215 <xuzhixian0902@gmail.com>
2024-03-18 15:05:36 +1100

    Added toggling star button

--- Piano/ContentView.swift
+++ Piano/ContentView.swift
@@ -8,44 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isStarClicked = false
     var body: some View {
         ZStack{
             Color(.systemMint)
                 .ignoresSafeArea()
             
+            
             VStack{
-                HStack{
-                    Image(systemName: "star")
-                    Text("Starry night")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                    Image(systemName: "star.fill")
+                VStack{
+                    HStack{
+                        Image(systemName: "star")
+                        Text("Starry night")
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Image(systemName: "star.fill")
+                    }
+                    
+                    Image("starry")
+                        .resizable()
+                        .cornerRadius(20)
+                        .aspectRatio(contentMode:.fit)
+                        .padding(.all)
+                    
+                    //Review stars
+                    HStack{
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
+                        Image(systemName: "star")
+                        Image(systemName: "star")
+                    }.foregroundColor(.black)
+                    
+                    Text("(500 reviews)")
+                    
                 }
+                .padding()
+                .background(Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15))
+                .padding()
                 
-                Image("starry")
-                    .resizable()
-                    .cornerRadius(20)
-                    .aspectRatio(contentMode:.fit)
-                    .padding(.all)
                 
-                //Review stars
-                HStack{
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.fill")
-                    Image(systemName: "star.leadinghalf.filled")
-                    Image(systemName: "star")
-                    Image(systemName: "star")
-                }.foregroundColor(.black)
-                
-                Text("(500 reviews)")
+                Button(action: {
+                                // Add your button action code here
+                                self.isStarClicked.toggle()
+                                print("Button tapped!")
+                            }) {
+                                Image(systemName: isStarClicked ? "star.fill" : "star")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 50, height: 50)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(25)
+                            }
                 
             }
-            .padding()
-            .background(Rectangle()
-                .foregroundColor(.white)
-                .cornerRadius(15)
-                .shadow(radius: 15))
-            .padding()
+            
             
         }
         
